a4effea88189ccfdcd77406d3e108deb60d88825
Xieqin You <yxqcynthia@gmail.com>
2017-03-17 21:38:56 -0500

    created "results" handler under SearchController to process search request and then added results-display function in search.html

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,31 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
-
+    @RequestMapping(value="results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<HashMap<String, String>> allJobs = JobData.findAll();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        if (searchType.equals("all")) {
+            for (HashMap<String, String> row : allJobs){
+                for (String column : row.keySet()){
+                    String aValue = row.get(column);
+                    if (aValue.matches("(?i:.*" + searchTerm + ".*)")){
+                        if (!jobs.contains(row)){
+                            jobs.add(row);
+                        }
+                    }
+                }
+            }
+        }else {
+            for (HashMap<String, String> row : allJobs) {
+                String aValue = row.get(searchType);
+                if (aValue.matches("(?i:.*" + searchTerm + ".*)")) {
+                    jobs.add(row);
+                }
+            }
+        }
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("jobs",jobs);
+        return "search";
+    }
 }
