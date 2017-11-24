6f58a6e2b66d0f565517d2cb719771cd2614aa6d
polarysekt <geoff@gd2>
2017-11-24 17:14:44 -0600

    begin messing with listing/searching

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -19,9 +19,34 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute( "listTitle", "All Jobs" );
+        model.addAttribute( "jobs", JobData.findAll() );
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping( value="results" )
+    public String results( Model model, @RequestParam String searchType, @RequestParam String searchTerm ) {
+        String strSearchType = new String();
+        ArrayList<HashMap<String,String>> alResults = JobData.findAll();
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute( "jobs", alResults );
 
+        String strResTitle = "Showing %d Results for \"%s\" in %s.";
+        switch(searchType) {
+            case "Position":
+            case "All":
+                strSearchType = searchType;
+                break;
+            default:
+                strSearchType = "All";
+                break;
+
+        }
+        // searchTerm
+        strResTitle = String.format(strResTitle, alResults.size(), searchTerm, strSearchType );
+
+        model.addAttribute( "listTitle", strResTitle);
+        return "search";
+    }
 }
