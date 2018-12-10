15fc54b8b7f9deaa0f7a7a064c3a0e04cf83fb0e
Bradley Boggs <bradleywboggs@gmail.com>
2018-12-09 21:04:20 -0600

    improved formatting of search results, refactored search thythymeleaf code for displaying search results into a fragment for reuse in other parts of the apof the application

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+
 
 /**
  * Created by LaunchCode
@@ -21,15 +21,6 @@ public class SearchController {
     static String negativeSearchResult;
     static ArrayList<HashMap<String, String>> jobsResults = new ArrayList<>();
 
-    static String printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        for (HashMap<String, String> job : someJobs) {
-            for (Map.Entry<String, String> category : job.entrySet()) {
-                return category.getKey() + " : " + category.getValue();
-
-            }
-        }return "";
-    }
-
 
     @RequestMapping(value = "")
     public String search(Model model) {
@@ -39,34 +30,36 @@ public class SearchController {
     }
 
     @RequestMapping(value = "/results", method = RequestMethod.GET)
-    public String redirectToSearch(Model model){
+    public String redirectToSearch(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         model.addAttribute("title", "Search");
         return "redirect:";
     }
+
     @RequestMapping(value = "/results", method = RequestMethod.POST)
     public String processSearchRequest(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
-        model.addAttribute("title", "Results");
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("title", "Search");
         if (searchType.equals("all")) {
             if (JobData.findByValue(searchTerm).isEmpty()) {
                 negativeSearchResult = "No matching jobs";
                 model.addAttribute("negativeSearchResult", negativeSearchResult);
-                return "search-results";
+                return "search";
             } else {
                 jobsResults = JobData.findByValue(searchTerm);
                 model.addAttribute("jobsResults", jobsResults);
-                return "search-results";
+                return "search";
             }
 
         } else {
             if(JobData.findByColumnAndValue(searchType, searchTerm).isEmpty()) {
                 negativeSearchResult = "No matching jobs";
                 model.addAttribute("negativeSearchResult", negativeSearchResult);
-                return "search-results";
+                return "search";
             } else {
                 jobsResults = JobData.findByColumnAndValue(searchType, searchTerm);
                 model.addAttribute("jobsResults", jobsResults);
-                return "search-results";
+                return "search";
             }
         }
 
